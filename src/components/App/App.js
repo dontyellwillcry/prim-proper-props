efc51da75a94f13e0fe5c02c3e7724129771c79c
LazyDotExe <papaporo@proton.me>
2023-08-10 01:07:44 -0500

    ?

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -63,9 +63,13 @@ function App() {
       <h2>Party Leader</h2>
       {guestList[0] && <h3>{guestList[0].name}</h3>}
       <h2>Add a new guest</h2>
+      {/* add form */}
       <GuestForm
-      getGuests={getGuests}
-      
+      handleSubmit={handleSubmit}
+      newGuestName={newGuestName}
+      setNewGuestName={setNewGuestName}
+      newGuestMeal={newGuestMeal}
+      setNewGuestMeal={setNewGuestMeal}
 
       />
       <h2>Guest List</h2>
