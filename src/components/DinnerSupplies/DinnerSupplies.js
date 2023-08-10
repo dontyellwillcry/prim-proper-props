f389c62dce0a6139feff1222d6701c2e6d38fd7b
LazyDotExe <papaporo@proton.me>
2023-08-10 00:46:16 -0500

    Added a GuestForm component

---
+++ src/components/DinnerSupplies/DinnerSupplies.js
@@ -0,0 +1,19 @@
+function DinnerSupplies({guestList}) {
+    return (
+        <>
+        <div>
+        Spoons: {guestList.length * 2}
+      </div>
+      <div>
+        Forks: {guestList.length * 2}
+      </div>
+      <div>
+        Knives: {guestList.length * 2}
+      </div>
+        </>
+    )
+}
+
+
+
+export default DinnerSupplies;
